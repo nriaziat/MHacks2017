fe2371ada19f1c8519c2fde76821a0036283a60a
nriaziat <nriaziat@gmail.com>
2017-03-25 23:28:03 +0000

    improved reliability

--- motorControl.ino
+++ motorControl.ino
@@ -2,7 +2,6 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 // Or, create it with a different I2C address (say for stacking)
@@ -21,17 +20,15 @@ void setup(){
   Serial.begin(9600);
   AFMS.begin();
   myMotor->setSpeed(80); 
-  pinMode(13, OUTPUT);
 }
 
 void loop(){
-  digitalWrite(led, LOW);
   recvInfo();
 }
   
   
 void recvInfo(){
-  if (Serial.available() > 0){
+  if (1){
     receivedChar = Serial.read();
     int val = receivedChar - '0';
     if (val == 1){
@@ -41,10 +38,7 @@ void recvInfo(){
 }
 
 void motorFunction(){
-
   myMotor->step(400, FORWARD, SINGLE); 
   myMotor->step(400, BACKWARD, SINGLE);
-  digitalWrite(led, HIGH);
-  delay(1000);
 }
 
