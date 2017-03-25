15c9064b18cbcc95f037191ce452377cfa08f78f
Shashank Murching <smurch@umich.edu>
2017-03-25 05:15:48 -0400

    Basis for Android App
    Super raw right now

---
+++ PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/Pill.java
@@ -0,0 +1,26 @@
+package com.example.shashank.pilldispenser;
+
+/**
+ * Created by Shashank on 3/25/2017.
+ */
+
+
+import java.awt.*;
+import java.util.Calendar;
+
+public class Pill {
+
+    private String name;
+    private int quantity;
+    private boolean meal;
+    private int hour;
+    private int minute;
+
+
+    Pill(){
+
+
+    }
+
+
+}
---
+++ PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/UserInfo.java
@@ -0,0 +1,14 @@
+package com.example.shashank.pilldispenser;
+
+/**
+ * Created by Shashank on 3/25/2017.
+ */
+
+public class UserInfo {
+
+    private int
+}
+
+
+
+
