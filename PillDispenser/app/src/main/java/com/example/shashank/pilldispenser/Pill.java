3acdd5c955633b815452d896c5711ece1710c742
Shashank Murching <smurch@umich.edu>
2017-03-25 13:44:02 -0400

    Updated Pill Class
    Added basic setter/getter functions in

--- PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/Pill.java
+++ PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/Pill.java
@@ -10,17 +10,65 @@ import java.util.Calendar;
 
 public class Pill {
 
-    private String name;
-    private int quantity;
+    private String name; //Name of pill
+    private int quantity; //How many pills loaded
+    private int output; //How many to dispense
     private boolean meal;
-    private int hour;
-    private int minute;
+    private int[] minutes; // Times to dispense in minutes
+    private boolean[] days; // 0 = Sunday, 1 = Monday, 2 = Tuesday, 3 = Wednesday, 4 = Thursday, 5 = Friday, 6 = Saturday
 
 
-    Pill(){
+    Pill(){}
 
-
+    /////////////////Initial Set Functions//////////////////////////////////////////////////////////
+    public void setName(String name) {
+        this.name = name;
     }
 
+    public void setQuantity(int quant) {
+        this.quantity = quant;
+    }
+
+    public void setOutput(int dispense){
+        this.output = dispense;
+    }
+
+    public void setMinutes(int[] times) {
+        this.minutes = times;
+    }
+
+    public void setMeal(boolean eatWithMeal) {
+        this.meal = eatWithMeal;
+    }
+
+    public void setDays(boolean[] days) {
+        this.days = days;
+    }
+
+    /////////////////Update Functions///////////////////////////////////////////////////////////////
+    public void updateQuant(int added) {
+        this.quantity += added;
+    }
+
+    /////////////////Get Functions//////////////////////////////////////////////////////////////////
+    public String getName() {
+        return this.name;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public int getOutput() {
+        return this.output;
+    }
+
+    public boolean getMeal() {return this.meal;}
+
+    public int[] getTime() {return this.minutes;}
+
+    public boolean[] getDays() {return this.days;}
+
+    ////////////////Other Functions///////////////////////////////////////////////////////////////
 
 }
--- PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/UserInfo.java
+++ PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/UserInfo.java
@@ -1,12 +1,17 @@
 package com.example.shashank.pilldispenser;
 
+import java.util.List;
+
 /**
  * Created by Shashank on 3/25/2017.
  */
 
 public class UserInfo {
 
-    private int
+    private List<Pill> l;
+
+
+
 }
 
 
