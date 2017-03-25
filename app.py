913002accc96ee7528bf6e285864b5f3f7a0e4e6
U-DESKTOP-SD9DPEH\naveed <nriaziat@gmail.com>
2017-03-25 13:53:54 -0400

    add alexa app

---
+++ app.py
@@ -0,0 +1,31 @@
+import boto
+import boto.s3
+import sys, time
+from boto.s3.key import Key
+from flask import Flask
+from flask_ask import Ask, statement, session, question
+import datetime
+import os
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+AWS_ACCESS_KEY = ''
+AWS_SECRET_KEY = ''
+bucket_name = 'trigfile'
+
+@ask.intent("TRIGGER")
+def upload():
+    conn = boto.connect_s3(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+
+    bucket = conn.get_bucket(bucket_name)
+
+    testfile = 'triggered'
+    k = Key(bucket)
+    k.key = testfile
+    k.set_contents_from_string('{:%m%d%Y%H%M%S}'.format(datetime.datetime.now()))
+
+    return statement('Dispensing!')
+
+if __name__ == '__main__':
+    app.run()
