8ef7aaac20a0976133a14f0ed26d96b8fffda2d2
U-DESKTOP-SD9DPEH\naveed <nriaziat@gmail.com>
2017-03-26 01:05:18 -0400

    added quantity function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import boto
 import boto.s3
-import sys, time
+import sys, time, urllib2
 from boto.s3.key import Key
 from flask import Flask
 from flask_ask import Ask, statement, session, question
@@ -16,7 +16,6 @@ bucket_name = 'trigfile'
 
 @ask.intent("TRIGGER")
 def upload():
-    bucket_name = 'trigfile'
     conn = boto.connect_s3(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 
     bucket = conn.get_bucket(bucket_name)
@@ -30,17 +29,21 @@ def upload():
 
 @ask.intent("QUANTITY")
 def qty():
-    conn = boto.connect_s3(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 
-    bucket = conn.get_bucket(bucket_name)
+    response = urllib2.urlopen("https://s3.amazonaws.com/trigfile/quantity")
+    quant = response.read()
+    response.close()
+    qtyArr = []
 
-    testfile = 'triggered'
-    k = Key(bucket)
-    k.key = testfile
-    k.set_contents_from_string('{:%m%d%Y%H%M%S}'.format(datetime.datetime.now()))
+    for i in range(1, 5):
+        findThis= "Tray"+str(i)
+        place = quant.find(findThis)
+        qtyArr.append(int(quant[place+6]))
 
-    return statement('Dispensing!')
-    
+    phrase = ("You have %d left in slot 1, %d left in slot 2, %d left in slot 3,"
+                        "and %d left in slot 4." %(qtyArr[0], qtyArr[1], qtyArr[2], qtyArr[3]))
+
+    return statement(phrase)
 
 if __name__ == '__main__':
     app.run()
