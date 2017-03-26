e6e59bf836d0a31d933e10b6cbe1ebfde1b0ed06
nriaziat <nriaziat@gmail.com>
2017-03-26 05:50:53 +0000

    cleanup

--- triggers.py
+++ triggers.py
@@ -2,13 +2,14 @@ import serial
 import urllib2
 from twilio.rest import TwilioRestClient
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM1', 9600)
 prevResponse = ''
 ser.write('0')
 account_sid = "ACd2452950046c0b6a89c8c455019e9754"
 auth_token = "0e6d95210e01bc0e3590afb558460bdd"
 client = TwilioRestClient(account_sid, auth_token)
 prevResponseQuantity = ""
+prevResponsePattern = ''
 dataQuantity = "0"
 
 
@@ -45,7 +46,7 @@ while True:
             count = 0
             dataQuantity = responseQuantity.read()
             responseQuantity.close()
-            if(prevResponseQuantity != dataQuantity):
+            if(prevResponseQuantity != dataQuantity and prevResponseQuantity != ''):
                 string = ""
                 for i in range(1, 5):
                     findThis= "Tray"+str(i)
@@ -62,13 +63,24 @@ while True:
                             string += str(i)
                             string += "=Tray"
                             string += str(i)
-                print(string)
+                #print(string)
                 call = client.calls.create(to="+15109363524",  # Any phone number
                                from_="+15104910043", # Must be a valid Twilio number
                                url="https://handler.twilio.com/twiml/EH6fbc3c785b3d84e49c7d495bb42299ba"+string)
                 #print(call.sid)
 
-                prevResponseQuantity = dataQuantity
+            prevResponseQuantity = dataQuantity
+        try:
+            responsePattern = urllib2.urlopen('https://s3.amazonaws.com/trigfile/pattern')
+        except urllib2.URLError:
+            pass
+        else:
+            dataPattern = responsePattern.read()
+            responsePattern.close()
+            if prevResponsePattern != dataPattern and prevResponsePattern != '': 
+                message = client.messages.create(to="+15109363524", from_="+15104910043",
+                                     body="You have pills ready to be taken")
+            prevResponsePattern = dataPattern
 
         #conn = boto.connect_s3(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 
