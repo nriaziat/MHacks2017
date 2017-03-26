2ad4853a5e8025df9b6edf279e24a40406195f0b
Shashank Murching <smurch@umich.edu>
2017-03-26 00:24:17 -0400

    Actually shows the UI now without Crashing

--- AndroidApp/PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/MainActivity.java
+++ AndroidApp/PillDispenser/app/src/main/java/com/example/shashank/pilldispenser/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.shashank.pilldispenser;
 
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.v4.view.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +18,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
@@ -37,25 +44,16 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
+
         calendar = Calendar.getInstance();
 
-        amazonCogInit();
+        //amazonCogInit();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,7 +65,7 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void amazonCogInit() {
+    /*private void amazonCogInit() {
 
         // Initialize the Amazon Cognito credentials provider
         CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
@@ -91,9 +89,9 @@ public class MainActivity extends AppCompatActivity
                 //Your handler code here
             }
         });
-    }
+    }*/
 
-    public void onCheckboxClicked(View view) {
+    /*public void onCheckboxClicked(View view) {
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
 
@@ -148,7 +146,7 @@ public class MainActivity extends AppCompatActivity
                 }
                 break;
         }
-    }
+    }*/
 
 
 
@@ -188,25 +186,48 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        int viewId = item.getItemId();
 
-        } else if (id == R.id.nav_slideshow) {
+        switch (viewId) {
+            case R.id.add:
+                fragment = new AddFragment();
+                title  = "Add";
+                break;
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.removePill:
+                fragment = new RemoveFragment();
+                title = "Remove";
+                break;
 
-        } else if (id == R.id.nav_share) {
+            case R.id.schedule:
+                fragment = new ScheduleFragment();
+                title = "Schedule";
+                break;
 
-        } else if (id == R.id.nav_send) {
+            case R.id.settings:
+                fragment = new SettingFragment();
+                title = "Settings";
+                break;
 
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_container, fragment);
+            ft.commit();
+        }
+
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
         return true;
     }
 }
